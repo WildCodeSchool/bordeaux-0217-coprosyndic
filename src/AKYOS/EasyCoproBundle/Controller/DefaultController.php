f0047c3a7b1cfbb8644c23ab1663051780f7f21c
L. S. Ratsimba <ia@shinoa.tv>
2017-05-23 15:49:26 +0200

    Revert "Formulaire de Contact"

--- src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
+++ src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
@@ -6,30 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-
     public function indexAction()
     {
         return $this->render('AKYOSEasyCoproBundle:FrontOffice:index.html.twig');
-    }
-
-    public function SubmitFormAction()
-    {
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Formulaire reception')
-            ->setFrom('send@example.com')
-            ->setTo('wcs.coprosyndic@gmail.com')
-            ->setBody($this->renderView(
-                'Emails/formulaire.html.twig', array(
-                        'name' => $_POST['name'],
-                        'email' => $_POST['email'],
-                        'tel' => $_POST['tel'],
-                        'message' => $_POST['message']
-                )),'text/html'
-            )
-        ;
-
-        $this->get('mailer')->send($message);
-
-        return $this->redirectToRoute('akyos_easy_copro_homepage');
     }
 }
