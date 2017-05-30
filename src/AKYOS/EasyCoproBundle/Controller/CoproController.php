667c673a065fb9aa7ba2b3ab23057202ccad8495
Virginie3377 <virginiecapdevielle77@gmail.com>
2017-05-30 16:28:44 +0200

    Système de connexion pour chaque type de compte

---
+++ src/AKYOS/EasyCoproBundle/Controller/CoproController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AKYOS\EasyCoproBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CoproController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('@AKYOSEasyCopro/BackOffice/Copro/index.html.twig');
+    }
+}
--- src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
+++ src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('AKYOSEasyCoproBundle:FrontOffice:index.html.twig');
-    }
+
+
+
+
+
+
+
 
     public function SubmitFormAction()
     {
