c8cdea22be2491e066a6396b4b0a2c5fda59c0d8
ia@shinoa.tv <ia@shinoa.tv>
2017-05-30 14:41:26 +0200

    Modifs appellations Controllers, routes, etc ..

--- src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
+++ src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
@@ -16,25 +16,5 @@ class DefaultController extends Controller
         return $this->render('AKYOSEasyCoproBundle:FrontOffice:mentions.html.twig');
     }
 
-    public function SubmitFormAction()
-    {
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Formulaire reception')
-            ->setFrom('send@example.com')
-            ->setTo('wcs.coprosyndic@gmail.com')
-            ->setBody($this->renderView(
-                '@AKYOSEasyCopro/Emails/formulaire.html.twig', array(
-                'name' => $_POST['name'],
-                'email' => $_POST['email'],
-                'tel' => $_POST['tel'],
-                'message' => $_POST['message']
-            )),'text/html'
-            )
-        ;
-
-        $this->get('mailer')->send($message);
-
-        return $this->redirectToRoute('akyos_easy_copro_homepage');
-    }
 
 }
