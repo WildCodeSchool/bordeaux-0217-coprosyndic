23ab7222f9e02cce28beda4c2d6bc56dc467c3b3
JM Labesse <jeanmarie.labesse@gmail.com>
2017-05-31 15:09:51 +0200

    Edition, suppression comptes SYNDIC

--- src/AKYOS/EasyCoproBundle/Controller/AdminController.php
+++ src/AKYOS/EasyCoproBundle/Controller/AdminController.php
@@ -2,7 +2,6 @@
 
 namespace AKYOS\EasyCoproBundle\Controller;
 
-
 use AKYOS\EasyCoproBundle\Entity\Syndic;
 use AKYOS\EasyCoproBundle\Form\RegisterSyndicType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -24,18 +23,75 @@ class AdminController extends Controller
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $syndic->getUser()->setEnabled(true);
+            $syndic->getUser()->setEnabled(true)->setType("SYNDIC");
             $em = $this->getDoctrine()->getManager();
             $em->persist($syndic);
             $em->flush();
 
-            return $this->redirectToRoute('akyos_easy_copro_backend_admin_index');
-        }
+            $request->getSession()->getFlashBag()->add('info', 'Le compte SYNDIC a bien été crée.');
 
+            return $this->redirectToRoute('akyos_easy_copro_backend_admin_list_syndic');
+        }
 
         return $this->render('@AKYOSEasyCopro/BackOffice/Admin/create_syndic.html.twig', array(
             'form' => $form->createView(),
         ));
     }
 
+    public function listSyndicAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $syndics = $em->getRepository(Syndic::class)
+            ->findAll();
+
+        return $this->render('@AKYOSEasyCopro/BackOffice/Admin/list_syndic.html.twig', array(
+            'syndics' => $syndics,
+        ));
+    }
+
+    public function showSyndicAction(Syndic $syndic)
+    {
+        return $this->render('@AKYOSEasyCopro/BackOffice/Admin/show_syndic.html.twig', array(
+            'syndic' => $syndic,
+        ));
+    }
+
+    public function editSyndicAction(Request $request, Syndic $syndic)
+    {
+        $form = $this->createForm(RegisterSyndicType::class, $syndic);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('info', 'Le compte SYNDIC a bien été modifié.');
+
+            return $this->redirectToRoute('akyos_easy_copro_backend_admin_show_syndic', array(
+                'id' => $syndic->getId(),
+            ));
+        }
+
+        return $this->render('@AKYOSEasyCopro/BackOffice/Admin/edit_syndic.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function deleteSyndicAction(Request $request, Syndic $syndic)
+    {
+
+        if ($syndic !== null) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($syndic);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('info', 'Le compte SYNDIC a bien été supprimé.');
+
+            return $this->redirectToRoute('akyos_easy_copro_backend_admin_list_syndic');
+        }
+
+        $request->getSession()->getFlashBag()->add('info', "Ce compte SYNDIC n'existe pas !");
+
+        return $this->redirectToRoute('akyos_easy_copro_backend_admin_list_syndic');
+    }
 }
--- src/AKYOS/EasyCoproBundle/Form/EditSyndicType.php
+++ src/AKYOS/EasyCoproBundle/Form/EditSyndicType.php
@@ -2,18 +2,19 @@
 
 namespace AKYOS\EasyCoproBundle\Form;
 
+use FOS\UserBundle\Form\Type\ProfileType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class RegisterSyndicType extends AbstractType
+class EditSyndicType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('user', RegistrationType::class)
+            ->add('user', ProfileType::class)
             ->add('nom')
             ->add('statut')
             ->add('siret')
