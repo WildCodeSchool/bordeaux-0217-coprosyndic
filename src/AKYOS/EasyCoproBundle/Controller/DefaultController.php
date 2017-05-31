e04e74f9cbab57a4af1fd7a335303de3fcb1cfbd
ia@shinoa.tv <ia@shinoa.tv>
2017-05-31 16:24:46 +0200

    Création des formulaires pour la création D'Artisans, de Copropriétaires et de Locataires

--- src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
+++ src/AKYOS/EasyCoproBundle/Controller/DefaultController.php
@@ -7,13 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
 
-
-
-
-
-
-
-
     public function mentionsAction()
     {
         return $this->render('AKYOSEasyCoproBundle:FrontOffice:mentions.html.twig');
