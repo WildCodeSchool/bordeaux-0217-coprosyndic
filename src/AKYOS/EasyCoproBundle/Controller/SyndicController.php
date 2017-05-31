b4b6de377da501696376b77fe01e43dd29fbc632
ia@shinoa.tv <ia@shinoa.tv>
2017-05-31 17:17:01 +0200

    Mises à jour nommages de Routes et Créations ds formulaires d'édition

--- src/AKYOS/EasyCoproBundle/Controller/SyndicController.php
+++ src/AKYOS/EasyCoproBundle/Controller/SyndicController.php
@@ -13,18 +13,33 @@ class SyndicController extends Controller
         return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/index.html.twig');
     }
 
-    public function newArtisanAction()
+    public function addArtisanAction()
     {
-        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/newArtisan.html.twig');
+        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/addArtisan.html.twig');
     }
 
-    public function newCoproAction()
+    public function addCoproAction()
     {
-        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/newCopro.html.twig');
+        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/addCopro.html.twig');
     }
 
-    public function newLocataireAction()
+    public function addLocataireAction()
     {
-        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/newLocataire.html.twig');
+        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/addLocataire.html.twig');
+    }
+
+    public function editArtisanAction()
+    {
+        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/editArtisan.html.twig');
+    }
+
+    public function editCoproAction()
+    {
+        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/editCopro.html.twig');
+    }
+
+    public function editLocataireAction()
+    {
+        return $this->render('@AKYOSEasyCopro/BackOffice/Syndic/editLocataire.html.twig');
     }
 }
