a3947a7a5b41b5fbc568567bff0c9f5adac90583
Virginie3377 <virginiecapdevielle77@gmail.com>
2017-06-01 12:01:35 +0200

    Formulaire création copropriété

---
+++ src/AKYOS/EasyCoproBundle/Form/CoproprieteType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AKYOS\EasyCoproBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CoproprieteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('immatriculation')
+            ->add('dateImmat')
+            ->add('dateModifImmat')
+            ->add('adressePrinc')
+            ->add('adresseSec')
+            ->add('codePostal')
+            ->add('ville')
+            ->add('dateCreation')
+            ->add('dateDebutGestion')
+            ->add('parcelleCadastrale')
+            ->add('nbBatiments')
+            ->add('periodeConstruction')
+            ->add('nbLots')
+            ->add('dateReglement')
+            ->add('nbAscenseurs')
+            ->add('typeChauffage')
+            ->add('syndic')
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AKYOS\EasyCoproBundle\Entity\Copropriete'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'akyos_easycoprobundle_copropriete';
+    }
+
+
+}
