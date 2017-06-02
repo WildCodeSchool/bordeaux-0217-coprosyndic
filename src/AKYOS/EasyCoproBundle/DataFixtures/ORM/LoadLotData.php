633399928b2dbdf40bbf394ee5dd016ded877c7d
JM Labesse <jeanmarie.labesse@gmail.com>
2017-06-02 11:16:29 +0200

    Ajout fixtures Coproprietaire & Lot

---
+++ src/AKYOS/EasyCoproBundle/DataFixtures/ORM/LoadLotData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AKYOS\EasyCoproBundle\DataFixtures\ORM;
+
+use AKYOS\EasyCoproBundle\Entity\Lot;
+use AKYOS\EasyCoproBundle\Entity\Syndic;
+use AKYOS\EasyCoproBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadLotData implements FixtureInterface, ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $lots =
+            [
+                [
+                    "identifiant" => "B150",
+                    "type" => "appartement",
+                    "quote_part" => "33",
+                    "loue_act" => true,
+                ],
+                [
+                    "identifiant" => "C025",
+                    "type" => "cave",
+                    "quote_part" => "5",
+                    "loue_act" => false,
+                ],
+            ];
+
+        foreach ($lots as $lot) {
+
+            $newLot = new Lot();
+            $newLot
+                ->setIdentifiant($lot["identifiant"])
+                ->setType($lot["type"])
+                ->setQuotePart($lot["quote_part"])
+                ->setLoueAct($lot["loue_act"])
+            ;
+
+            $manager->persist($newLot);
+            $manager->flush();
+        }
+    }
+}
