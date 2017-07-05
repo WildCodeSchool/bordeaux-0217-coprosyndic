fd22ad084402d1d2cdd92e72a8adc0aa246f854a
ia@shinoa.tv <ia@shinoa.tv>
2017-07-05 15:01:13 +0200

    gestion des conflits

--- src/AKYOS/EasyCoproBundle/Entity/Copropriete.php
+++ src/AKYOS/EasyCoproBundle/Entity/Copropriete.php
@@ -144,7 +144,7 @@ class Copropriete
     private $typeChauffage;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      * @var string
      */
     private $illustrationName;
@@ -156,7 +156,7 @@ class Copropriete
     private $illustrationFile;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      *
      * @var \DateTime
      */
